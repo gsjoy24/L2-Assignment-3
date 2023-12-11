50e5147b1b565c9b0c0abf2b44e894d6a66bc780
gsjoy24 <gour.joy24@gmail.com>
2023-12-11 14:55:52 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express();
+
+// parsers
+app.use(express.json());
+app.use(cors());
+
+// test api
+app.get('/', (req: Request, res: Response) => {
+  res.send('server is running');
+});
+
+export default app;
