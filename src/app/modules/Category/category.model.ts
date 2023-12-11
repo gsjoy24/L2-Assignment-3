479a0abe11f99e266cc2a80ab43a1a69ef138aba
gsjoy24 <gour.joy24@gmail.com>
2023-12-11 15:55:32 +0600

    added category interface and model

---
+++ src/app/modules/Category/category.model.ts
@@ -0,0 +1,12 @@
+import { Schema, model } from 'mongoose';
+import { TCategory } from './category.interface';
+
+const CategorySchema = new Schema<TCategory>({
+  name: {
+    type: String,
+    required: [true, 'Category name is required'],
+    unique: true,
+    trim: true,
+  },
+});
+export const Category = model<TCategory>('Category', CategorySchema);
